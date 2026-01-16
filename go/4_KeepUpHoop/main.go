4113783df522d5f04fef200da0c1a86d7b431ee3
Hamadah <muhammadrozaan@gmail.com>
2026-01-16 08:23:49 +0700

    go: keep up hoop

---
+++ go/4_KeepUpHoop/main.go
@@ -0,0 +1,29 @@
+/*
+Keep Up Hoop
+
+kata: https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/go
+
+Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him.
+
+Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message:
+
+If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func HoopCount(n int) string {
+	if n >= 10{
+		return "Great, now move on to tricks"
+	}
+	return "Keep at it until you get it"  
+}
+
+func main() {
+	message := HoopCount(7)
+	fmt.Println(message)
+}
